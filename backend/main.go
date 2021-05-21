fa89205f4818b41c3d264ceaceaaab1c0dedcecb
metairie <metairie@ub20.04>
2021-05-21 19:15:08 +0200

    add pool of session for mongodb - to be improved later

--- backend/main.go
+++ backend/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	testMongodb()
+	// check the mongodb with track id = 1
+	getTrack("1")
 	log.Println("Start HTTP server")
 	// start http server
 	router := NewRouter(AllRoutes())
--- backend/mongodb.go
+++ backend/mongodb.go
@@ -1,78 +1,89 @@
 package main
 
 import (
-	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"log"
 	"os"
 )
 
-func postTrack(track Track) {
-	// -------------------------------------------------------------------
-	// ------------- mongodb session -------------------------------------
-	server := os.Getenv("MONGODB_HOST")
-	session, err := mgo.Dial(server)
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	session.Login(&mgo.Credential{Username: os.Getenv("MONGODB_USER"), Password: os.Getenv("MONGODB_PASSWORD"), Source: "stl"})
-	c := session.DB("stl").C("track")
-	// -------------------------------------------------------------------
-	// -------------------------------------------------------------------
+var sessions = make([]*Session, 0, MAX)
 
-	err = c.Insert(&Track{track.Id, track.Name, track.Author, track.Steps})
+const MAX = 99
+
+func postTrack(track Track) {
+	s := getSession()
+	s.Locked = true
+	coll := s.getCollection()
+	err := coll.Insert(&Track{track.Id, track.Name, track.Author, track.Steps})
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("postTrack session UUID:", s.Uuid)
+	s.Locked = false
 }
 
 func getTracks() []Track {
-	// -------------------------------------------------------------------
-	// ------------- mongodb session -------------------------------------
-	server := os.Getenv("MONGODB_HOST")
-	session, err := mgo.Dial(server)
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	session.Login(&mgo.Credential{Username: os.Getenv("MONGODB_USER"), Password: os.Getenv("MONGODB_PASSWORD"), Source: "stl"})
-	c := session.DB("stl").C("track")
-	// -------------------------------------------------------------------
-	// -------------------------------------------------------------------
-	result := []Track{}
-	err = c.Find(bson.M{}).All(&result)
+	results := []Track{}
+	s := getSession()
+	s.Locked = true
+	coll := s.getCollection()
+	err := coll.Find(bson.M{}).All(&results)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return result
+	log.Println("getTracks session UUID:", s.Uuid)
+	s.Locked = false
+	return results
 }
 
 func getTrack(id string) Track {
-	// -------------------------------------------------------------------
-	// ------------- mongodb session -------------------------------------
-	server := os.Getenv("MONGODB_HOST")
-	session, err := mgo.Dial(server)
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-	session.SetMode(mgo.Monotonic, true)
-	session.Login(&mgo.Credential{Username: os.Getenv("MONGODB_USER"), Password: os.Getenv("MONGODB_PASSWORD"), Source: "stl"})
-	c := session.DB("stl").C("track")
-	// -------------------------------------------------------------------
-	// -------------------------------------------------------------------
 	result := Track{}
-	err = c.Find(bson.M{"id": id}).One(&result)
+	s := getSession()
+	s.Locked = true
+	coll := s.getCollection()
+	err := coll.Find(bson.M{"id": id}).One(&result)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("getTrack session UUID:", s.Uuid)
+	s.Locked = false
 	return result
 }
 
-func testMongodb() {
-	getTrack("1")
-	log.Println("Mongodb OK server: ", os.Getenv("MONGODB_HOST"))
+func getSession() *Session {
+	// get next available session
+	countSessions := 0
+	countLockedSessions := 0
+	addSession := true
+	for _, s := range sessions {
+		if s != nil {
+			if s.Locked {
+				countLockedSessions++
+			} else {
+				addSession = false
+				break
+			}
+			countSessions++
+			addSession = true
+		}
+	}
+	if addSession && countSessions > MAX {
+		log.Fatal("No more connection available ", countSessions, "/", MAX, " reached")
+	}
+	if addSession && countSessions <= MAX {
+		s := New(os.Getenv("MONGODB_HOST"), "stl", os.Getenv("MONGODB_USER"), os.Getenv("MONGODB_PASSWORD"), "stl", "track")
+		sessions = append(sessions, s)
+		log.Println("New Session created: ", s.Uuid)
+	} else {
+		log.Println("Use existing connection")
+	}
+	return sessions[countSessions]
+}
+
+func closeDb() {
+	for _, s := range sessions {
+		if s != nil {
+			defer s.MgoSession.Close()
+		}
+	}
 }
