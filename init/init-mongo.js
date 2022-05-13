6c123cb60f61f8a73f52bff433f055b8ae8f99d5
DESKTOP-UM1GQI4\metairie <stephane.metairie@gmail.com>
2022-05-13 22:05:57 +0200

    version working

--- init/init-mongo.js
+++ init/init-mongo.js
@@ -1,6 +1,7 @@
+
 db.createUser({
-    user: 'root',
-    pwd: 'example',
+    user: 'admin',
+    pwd: 'secret',
     roles: [{role: "readWrite", db: "admin"}, {role: "dbAdmin", db: "admin"}]
 });
 
@@ -14,4 +15,7 @@ db.createUser({
 db.createCollection("tracks", {capped: false});
 db.tracks.insertOne({"id": 1, "name": "Track Test 1 - 1km", "author": "stluser", steps: 0});
 db.tracks.insertOne({"id": 2, "name": "Track Test 2 - 4.5kms", "author": "stluser", steps: 1});
-db.tracks.insertOne({"id": 3, "name": "Saintelyon - JIT 75kms", "author": "stluser", steps: 10});
+db.tracks.insertOne({"id": 3, "name": "Saintelyon 2019 - 76kms", "author": "stluser", steps: 10});
+db.tracks.insertOne({"id": 4, "name": "Saintelyon 2021 - JIT 75kms", "author": "stluser", steps: 10});
+db.tracks.insertOne({"id": 5, "name": "Saintelyon 2022 - 78kms", "author": "stluser", steps: 10});
+db.tracks.insertOne({"id": 6, "name": "Montée Sur Cou - 15kms", "author": "stluser", steps: 3});
